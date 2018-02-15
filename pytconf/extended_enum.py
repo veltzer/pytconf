d5087ad985301c56fc957d1a2358be485026b699
Mark Veltzer <mark.veltzer@gmail.com>
2018-02-15 12:28:13 +0200

    better extended enum

--- pytconf/extended_enum.py
+++ pytconf/extended_enum.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Type
 
 from pytconf.enum_subset import EnumSubset
 
@@ -15,3 +16,12 @@ class ExtendedEnum(Enum):
             enum_type=cls.__class__,
             list_of_values=cls.get_list_of_all_values()
         )
+
+    @classmethod
+    def from_string(cls, s):
+        # type: (str) -> Type[Enum]
+        for x in cls:
+            if s == x.name:
+                return x
+        raise ValueError("cannot find value [{}]".format(s))
+
