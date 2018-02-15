f46cf668b0bba97e13a78d5930c4b24df80b2b5a
Mark Veltzer <mark.veltzer@gmail.com>
2018-02-15 13:40:11 +0200

    more EnumSubset work

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ http://setuptools.readthedocs.io/en/latest/setuptools.html
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name='pytconf',
-    version='0.0.10',
+    version='0.0.11',
     packages=[
         'pytconf',
     ],
