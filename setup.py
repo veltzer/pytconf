a0ff7ba837146f8154cc12e16962082b7d5be846
Mark Veltzer <mark.veltzer@gmail.com>
2018-02-16 23:44:19 +0200

    new version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ http://setuptools.readthedocs.io/en/latest/setuptools.html
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name='pytconf',
-    version='0.0.13',
+    version='0.0.14',
     packages=[
         'pytconf',
     ],
