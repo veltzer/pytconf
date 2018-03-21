81bc0b27cd4fa14113faa90f4f005e612cdc628c
Mark Veltzer <mark.veltzer@gmail.com>
2018-03-21 16:22:00 +0200

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ http://setuptools.readthedocs.io/en/latest/setuptools.html
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name='pytconf',
-    version='0.0.18',
+    version='0.0.19',
     packages=[
         'pytconf',
     ],
