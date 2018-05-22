8dee43ace35d0dd2ab1105fb94057f650393360f
Mark Veltzer <mark.veltzer@gmail.com>
2018-05-22 17:32:54 +0300

    new version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ http://setuptools.readthedocs.io/en/latest/setuptools.html
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name='pytconf',
-    version='0.0.26',
+    version='0.0.27',
     packages=[
         'pytconf',
     ],
