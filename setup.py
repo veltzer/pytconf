5878593f7576c88e29e843f4e88e930a66298585
Mark Veltzer <mark.veltzer@gmail.com>
2020-05-30 22:39:52 +0300

    upgrade version for wheel generation

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ http://setuptools.readthedocs.io/en/latest/setuptools.html
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name='pytconf',
-    version='0.0.31',
+    version='0.0.32',
     packages=[
         'pytconf',
     ],
