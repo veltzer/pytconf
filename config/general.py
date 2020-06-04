601e1985ccf4c11733009daf9e2ddf5b3e759f4a
Mark Veltzer <mark.veltzer@gmail.com>
2020-06-04 05:05:19 +0300

    new template

--- config/general.py
+++ config/general.py
@@ -8,4 +8,4 @@ general_current_year = datetime.datetime.now().year
 general_homedir = os.path.expanduser("~")
 # d.general_hostname=subprocess.check_output(['hostname']).decode().rstrip()
 general_hostname = socket.gethostname()
-general_domain_name = subprocess.check_output(["hostname", "--domain"]).rstrip()
+general_domain_name = subprocess.check_output(["hostname", "--domain"]).decode().rstrip()
