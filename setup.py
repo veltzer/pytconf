ebd7daaf818b7f234de5292fc4e5cf6fac94b7e3
Mark Veltzer <mark.veltzer@gmail.com>
2020-06-06 14:36:59 +0300

    fixed up license

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setuptools.setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
     ],
     data_files=[
     ],
