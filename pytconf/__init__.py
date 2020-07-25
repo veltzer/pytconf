23dfbb10243734fae176c868002e0f3849fedd9c
Mark Veltzer <mark.veltzer@gmail.com>
2020-07-25 13:58:53 +0300

    trying again

--- pytconf/__init__.py
+++ pytconf/__init__.py
@@ -1,2 +1,8 @@
-from pytconf.config import Config
 from pytconf.param import ParamCreator
+from pytconf.config import (
+    Config,
+    register_main,
+    register_function,
+    register_function_group,
+    config_arg_parse_and_launch,
+)
