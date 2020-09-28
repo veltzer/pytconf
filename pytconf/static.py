c64b2d2c1b593e113747a1d01a47105d46e78465
Mark Veltzer <mark.veltzer@gmail.com>
2020-09-28 20:00:37 +0300

    new boilerplate

---
+++ pytconf/static.py
@@ -0,0 +1,4 @@
+""" version which can be consumed from within the module """
+VERSION_STR = "0.0.60"
+DESCRIPTION = "pytconf handles configuration, config files and command line parsing"
+APP_NAME = "pytconf"
