fd70a73c70a2d50de75e3a312d15e1b2726af6fa
Mark Veltzer <mark.veltzer@gmail.com>
2020-11-14 04:01:35 +0200

    better error printing

--- pytconf/errors_collector.py
+++ pytconf/errors_collector.py
@@ -4,9 +4,13 @@ from typing import List
 class ErrorsCollector:
     def __init__(self):
         self.errors: List[str] = []
+        self.flag_errors = False
 
     def add_error(self, msg: str) -> None:
         self.errors.append(msg)
 
+    def raise_error(self):
+        self.flag_errors = True
+
     def have_errors(self) -> bool:
-        return len(self.errors) > 0
+        return len(self.errors) > 0 or self.flag_errors
--- pytconf/static.py
+++ pytconf/static.py
@@ -1,4 +1,4 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.61"
+VERSION_STR = "0.0.62"
 DESCRIPTION = "pytconf handles configuration, config files and command line parsing"
 APP_NAME = "pytconf"
