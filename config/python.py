d8bb4117496437420bb01e8189554fea1b05ef1c
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-19 19:12:43 +0300

    new config

--- config/python.py
+++ config/python.py
@@ -1,7 +1,3 @@
-import config.project
-
-package_name = config.project.project_name
-
 dev_requires = [
     "pypitools",
     "pyclassifiers",
@@ -23,8 +19,3 @@ test_requires = [
     "pyflakes",
     "pylogconf",
 ]
-
-python_requires = ">=3.10"
-
-test_os = ["ubuntu-22.04"]
-test_python = ["3.10"]
