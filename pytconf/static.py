f877d664bdd55d26815ecdfcbb2addfc71abb592
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-29 00:31:51 +0300

    new pydmt

--- pytconf/static.py
+++ pytconf/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.84"
+VERSION_STR = "0.0.85"
 DESCRIPTION = "pytconf handles configuration and command line parsing"
 APP_NAME = "pytconf"
 LOGGER_NAME = "pytconf"
