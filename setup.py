08baf7020c4286590cdd58d62aec35ee9bdad0c0
Mark Veltzer <mark.veltzer@gmail.com>
2023-10-11 01:57:22 +0300

    new vesrion

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pytconf",
-    version="0.0.89",
+    version="0.0.90",
     packages=[
         "pytconf",
     ],
