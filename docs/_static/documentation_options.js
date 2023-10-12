bc4c97e6478ae530899bdf7f3eec8d7722086e6d
Mark Veltzer <mark.veltzer@gmail.com>
2023-10-12 19:30:54 +0300

    new version

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.95',
+    VERSION: '0.0.96',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pytconf",
-    version="0.0.95",
+    version="0.0.96",
     packages=[
         "pytconf",
     ],
