c7dbc5ec37bc08297d512f1e4d205efe84496eae
Mark Veltzer <mark.veltzer@gmail.com>
2023-10-12 21:25:26 +0300

    new version

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.97',
+    VERSION: '0.0.98',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- pytconf/static.py
+++ pytconf/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.97"
+VERSION_STR = "0.0.98"
 DESCRIPTION = "pytconf handles configuration and command line parsing"
 APP_NAME = "pytconf"
 LOGGER_NAME = "pytconf"
