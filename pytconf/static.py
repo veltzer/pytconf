fa91b752a38a53f2d6dd7f827277faa13bf1865a
Mark Veltzer <mark.veltzer@gmail.com>
2024-03-09 06:41:00 +0200

    new version

--- pytconf/static.py
+++ pytconf/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.1.13"
+VERSION_STR = "0.1.14"
 DESCRIPTION = "Configuration, command line and injection framework for python"
 APP_NAME = "pytconf"
 LOGGER_NAME = "pytconf"
