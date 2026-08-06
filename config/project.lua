385d59c5deeaba90df81546522336ac7cfe844e5
Mark Veltzer <mark.veltzer@gmail.com>
2026-08-06 12:09:22 +0300

    pydmt -> rsconstruct

---
+++ config/project.lua
@@ -0,0 +1,9 @@
+NAME = "pytconf"
+DESCRIPTION_SHORT = "Configuration, command line and injection framework for python"
+KEYWORDS = {
+    "click",
+    "argparse",
+    "optparse",
+    "command-line-parser",
+    "configuration",
+}
